41c69e6aad1a7094e427455e565b2695a2683c93
unknown <mckennafarmer2023@u.northwestern.edu>
2023-03-29 15:28:52 -0500

    solids parsing

--- scripts/cycle_parsing.R
+++ scripts/cycle_parsing.R
@@ -1,20 +1,28 @@
 ## ---------------------------
-## spreadsheet parsing script for CANDO+P data
-## import into Rmd or use standalone
+## spreadsheet parsing script for CANDO+P in-cycle sampling data
 ## ---------------------------
 
-library(readxl)
-library(lubridate)
-library(reshape2)
-library(dplyr)
+# library(readxl)
+# library(lubridate)
+# library(reshape2)
+# library(dplyr)
 
 cycle_parse <- function(file_path, choose_date){
   df <- read_excel(path = file_path, sheet = "data")
   
-  df_parsed <- df %>% filter(date == ymd(choose_date)) %>%
-    select(-c(date, time, phase, "NO2+NO3_mgNL")) %>%
-    melt(id.vars=c("hour", "date_time")) %>%
-    mutate(date_time = ymd_hms(date_time))
+  # calculate time in hour from beginning of cycle
+  df_parsed <- df %>% 
+    filter(date == ymd(choose_date)) %>%
+    mutate(hour = time_length(interval(min(time), time), "hour")) %>%
+    select(-c(date, time, "NO2+NO3_mgNL"))
+  
+  # long df for plotting
+  df_parsed <- melt(df_parsed, id = c("hour", "phase"))
+
+  
+  # order the factors and clean up names
+  df_parsed$phase <- factor(df_parsed$phase,
+                               levels=c("ana", "anx", "aer"))
   
   df_parsed$variable <- factor(df_parsed$variable,
                                levels=c("OP_mgPL","NO2_mgNL","NO3_mgNL",
@@ -30,7 +38,10 @@ cycle_parse <- function(file_path, choose_date){
 
 
 ## debug zone ---
+
 # file_path <- "C:/Users/mckyf/Northwestern University/Wells Research Group - CANDO+P Reactor/04 Performance data/cycle_results_2023.xlsx"
 # choose_date <- "2023-03-13"
-
 # cycle_parse(file_path, choose_date)
+
+
+
---
+++ scripts/plotting.R
@@ -0,0 +1,22 @@
+## ---------------------------
+## plotting params for CANDO+P
+## ---------------------------
+
+library(ggplot2)
+library(dplyr)
+
+theme_defaults <- list(
+  theme_classic(),
+  theme(legend.position="top", plot.title = element_text(face = "bold")))
+
+
+# variable dictionary
+## this is actually a df since R doesnt natively support dicts
+## make ggplot custom colors/shapes on the fly
+
+palette_df <- tibble(vars = c("phosphate", "nitrite", "nitrate", "acetate", "propionate", "glucose"),
+                     labels = c("Phosphate", "Nitrite", "Nitrate", "Acetate", "Propionate", "Glucose"),
+                     shapes = c(16, 17, 18, 21, 22, 25),
+                     colors = c("#92A36A", "#D27A37", "#E1A77A", "#EB99B8", "#AA2256", "#802E35"),
+                     fill = c("#92A36A", "#D27A37", "#E1A77A", "grey", "grey", "grey"))
+
