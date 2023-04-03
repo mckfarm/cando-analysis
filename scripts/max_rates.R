57c6bb000c45fcde4cf73fc413eb3112a676e847
unknown <mckennafarmer2023@u.northwestern.edu>
2023-04-03 11:05:13 -0500

    quarto deploy

---
+++ scripts/max_rates.R
@@ -0,0 +1,34 @@
+## ---------------------------
+## max rate calculations
+## ---------------------------
+
+# use this script assuming you have an in-cycle plot with hour, phase, variable, and value columns 
+
+# library(dplyr)
+
+get_max_rate <- function(df, choose_phase, choose_variable, n_points, solids_df){
+  
+  # filter values
+  df_select <- df %>%
+    filter(phase == choose_phase) %>%
+    filter(variable == choose_variable) %>%
+    slice(1:n_points)
+
+  lm_ana_phos <- lm(value ~ hour, data = df_select)
+  
+  max_rate <- abs(unname(coef(lm_ana_phos)["hour"]))
+  
+  # normalize rate if solids df is supplied, else return non-normalized rate
+  
+  if(missing(solids_df)) {
+    return(max_rate)
+  } else {
+      max_norm_rate <- max_rate / filter(solids, sample == "ml")$TSS
+      return(max_norm_rate)
+  }
+  
+} 
+
+## debug zone ---
+
+
